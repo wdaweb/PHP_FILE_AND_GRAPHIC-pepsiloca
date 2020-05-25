49e92514a85aa996c324626062402beff0a034e9
u8931011 <u8931011@gmail.com>
2020-05-25 11:50:37 +0800

    寫入資料表及匯出成文字檔

--- parse_file.php
+++ parse_file.php
@@ -11,19 +11,32 @@ if(!empty($_FILES['doc']['tmp_name'])){
     echo "檔案搬移位置在"."doc/".$_FILES['doc']['name']; */
 
 
-    if($_FILES['doc']['type']=='text/plain'){
+    if($_FILES['doc']['type']=='text/plain'){ //文字格式才接受
         move_uploaded_file($_FILES['doc']['tmp_name'],'doc/'.$_FILES['doc']['name']);
 
         $path='doc/'.$_FILES['doc']['name'];
 
         $file=fopen($path,"r+");
 
+        //$txt=fgets($file);
+        $num=1;
         while(!feof($file)){
             $txt=fgets($file);
-            $font_size=rand(12,24)."px";
-            echo "<div style='margin:10px;font-size:$font_size'>".$txt."</div>";
+            $tmp=explode(",",$txt);
+            if(count($tmp)==4){
+                $content['subject']=$tmp[0];
+                $content['description']=$tmp[1];
+                $content['create_date']=$tmp[2];
+                $content['due_date']=$tmp[3];
+                //print_r($tmp);
+                save("todo_list",$content);
+                //echo "已儲存".$num."筆資料";
+                
+                $num++;
+            }
         }
 
+        to("text-import.php");
         
 
     }else{
@@ -34,6 +47,10 @@ if(!empty($_FILES['doc']['tmp_name'])){
 }else{
     echo "上傳錯誤:".$_FILES['doc']['error'];
 }
+
+
+
+
 
 
 ?>
